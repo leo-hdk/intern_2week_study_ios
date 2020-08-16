6fe23af37acc52678887394efaa20bb9440e0712
leo-hdk <leo.syuukatu@gmail.com>
2020-08-17 00:05:27 +0900

    課題2-1の修正 課題2-2の追加# Please enter the commit message for your changes. Lines starting

--- intern_2week_study/Question/Question2/Question2ViewController.swift
+++ intern_2week_study/Question/Question2/Question2ViewController.swift
@@ -3,11 +3,23 @@ import UIKit
 final class Question2ViewController: UIViewController,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    //@IBOutlet weak var imageView: UIImageView!
     
     private let areaTexts: [String] = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"]
     
+    private let areaImages: [String] = [
+    "https://uub.jp/47/ibaraki/ibaraki_kensho.png",
+    "https://uub.jp/47/tochigi/tochigi_kensho.png",
+    "https://uub.jp/47/gunma/gunma_monsho.png",
+    "https://uub.jp/47/saitama/saitama_kensho.png",
+    "https://uub.jp/47/chiba/chiba_kensho.png",
+    "https://uub.jp/47/tokyo/tokyo_symbol.png",
+    "https://uub.jp/47/kanagawa/kanagawa_kensho.png"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         tableView.register(UINib(nibName: "Question2Cell", bundle: nil), forCellReuseIdentifier: "question2Cell")
     }
 }
 
@@ -18,12 +30,14 @@ extension Question2ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "question2Cell", for: indexPath) as? Question2Cell {
+            
+            cell.setCellImageAndLabel(labelText: areaTexts[indexPath.row], imageURL: URL(string: areaImages[indexPath.row])!)
+            return cell
+        }
         
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
+        return UITableViewCell()
         
-        let label1 = cell.viewWithTag(2) as? UILabel
-        label1?.text = String(describing: areaTexts[indexPath.row])
-        
-        return cell
     }
 }
